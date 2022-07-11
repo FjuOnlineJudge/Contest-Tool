0dc7c091d14907105ad326fdea03e3205e758cc7
allem40306 <allem40306@gmail.com>
2022-07-11 15:25:02 +0800

    add tex template and modify .gitignore

--- script/makePdf.sh
+++ script/makePdf.sh
@@ -1,1 +1,4 @@
-sudo docker exec -it pdfMaker /bin/bash -c "texliveonfly main.tex --compiler=xelatex ; latexmk -pdfxe main.tex"
+docker unpause pdfMaker
+docker exec -it pdfMaker bash -c "texliveonfly problem.tex --compiler=xelatex ; latexmk -pdfxe problem.tex"
+docker pause pdfMaker
+rm problem.[^tp]* texput.log
